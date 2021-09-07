fee7fbfa93b9aad795459ac1e810a2c8b03626ad
adel <vafin.adel04@gmail.com>
2021-09-07 20:54:20 +0300

    +status

--- college/models/student.py
+++ college/models/student.py
@@ -10,3 +10,16 @@ class CollegeStudent(models.Model):
     birthday = fields.Date(string='Birthday')
     country = fields.Many2one('res.country', 'Country')
     group = fields.Many2one("college.group", string="Group")
+    phone = fields.Text(string="Phone")
+    mail = fields.Text(string="Email")
+    image = fields.Image(string='Image')
+    state = fields.Selection(
+        string='State',
+        selection=[
+            ('enrollee', 'Enrolle'),
+            ('student', 'Student'),
+            ('alumnus', 'Alumnus'),
+            ('expelled', 'Expelled'),
+        ],
+        default='enrollee'
+    )
